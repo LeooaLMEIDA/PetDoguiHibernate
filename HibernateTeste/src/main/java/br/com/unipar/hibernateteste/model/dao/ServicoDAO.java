54a45f2e649243ef9fead363c564b21098470046
Leonardo Almeida <88916540+LeooaLMEIDA@users.noreply.github.com>
2023-04-29 22:41:28 -0300

    Add files via upload

---
+++ HibernateTeste/src/main/java/br/com/unipar/hibernateteste/model/dao/ServicoDAO.java
@@ -0,0 +1,9 @@
+package br.com.unipar.hibernateteste.model.dao;
+
+import br.com.unipar.hibernateteste.model.Servico;
+
+public class ServicoDAO extends GenericDAO<Servico>{
+  public ServicoDAO() {
+        super(Servico.class);
+    }
+}
